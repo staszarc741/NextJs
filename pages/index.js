6269b3c27c2b30aaff297df560db4dd0f28207c6
Syas Zarc <staszarc741@gmail.com>
2022-05-20 15:03:31 +0300

    app created #1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
+import {
+  Heading,
+  VStack,
+  HStack,
+  Icon,
+  Text,
+  Link,
+  Avatar
+} from '@chakra-ui/react';
 
+import { FaTwitter, FaGithub, } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <h1>
-      Home
-    </h1>
+    <VStack align="center" gap="32px" justify="center">
+      <Avatar src="/anime-dom.png" size="2xl" />
+      <VStack>
+        <Heading as="h1">Stas *Teis* Zayets</Heading>
+        <Heading size="md" as="h3">DX Engineer & Community creator</Heading>
+      </VStack>
+      <Text textAlign="center">👋 heyo! My names Domitrius. Ive been a UI/DX Engineer over the past 5 years. Finding solutions while improving someones ability to vist or build websites is my passion. I love video games, anime, and spending my time with those I love. <br /> Lets connect 👇</Text>
+      <HStack spacing="20px" justifySelf="center">
+        <Link href="https://twitter.com/staszarc" target="_blank">
+          <Icon color="blue.200" as={FaTwitter} />
+        </Link>
+        <Link href="https://github.com/staszarc741" target='_blank'>
+          <Icon color="black" as={FaGithub} />
+        </Link>
+      </HStack>
+    </VStack>
   )
 }
---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+
+import { VStack } from "@chakra-ui/react";
+import ProjectCard from '@components/ProjectCard';
+
+export default function Projects() {
+  return (
+    <VStack spacing={10} mt={10} justify="flex-start" direction="column">
+      <ProjectCard
+        image="/portfolio.png"
+        name="Portfolio"
+        link="https://github.com/domitriusclark/portfolio"
+      />
+    </VStack>
+  )
+}
